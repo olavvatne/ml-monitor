110027360cc0f0592f5707f01397a49bf018791f
Olav Vatne <olavvatne@gmail.com>
2016-01-19 10:12:07 +0100

    Create js folder if not exists

--- src/server/server.js
+++ src/server/server.js
@@ -49,6 +49,7 @@ var createFolder = function(path, name) {
 };
 
 createFolder('./public/', 'style');
+createFolder('./public/', 'js');
 moveFile('./node_modules/muicss/dist/css/mui.min.css', './public/style/mui.min.css');
 moveFile('./node_modules/muicss/dist/js/mui.min.js', './public/js/mui.min.js');
 moveFile('./node_modules/normalize.css/normalize.css', './public/style/normalize.css');
